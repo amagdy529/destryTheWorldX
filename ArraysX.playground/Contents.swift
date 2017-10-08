4cd28675c302bee9916a59fac849721ff218fce3
Amr Elsayed <amrelsayed@Brights-Mac-mini-2.local>
2017-10-08 19:16:07 +0200

    first commit x

---
+++ ArraysX.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var employee1Salary = 45000
+var employee2Salary = 55000
+
+
+// array of type int fuck yeah !
+var employeesSalaries : [Int] = [45,44,44,44,44]
+
+var employeesSalaries3 : [Double] = [45,44,44,44,44]
+
+var employeesSalaries5 : Array<Double> = [45,44,44,44,44]
+
+
+// array of type int fuck yeah !
+var employeesSalaries2:Any = [45,44,44,44,44 , "aaaa"]
+
+
+employeesSalaries.count
+employeesSalaries.description
+employeesSalaries.append(55555)
+employeesSalaries.count
+employeesSalaries.remove(at: 0) // index zero
+employeesSalaries.count
+
+
+var students = [String]()
+
+students.append("ahmed")
+
+var student2 = [String]
+
+student2.append("haha")
+
